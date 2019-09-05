a2cb255d7a8bbae9267bbe448796e55640a8380a
Ian Martin <christian_martin@outlook.com>
2019-09-06 07:48:03 +0800

    Additional model setup to establish all the data sets and data manipulations

--- app/TaxIncome.php
+++ app/TaxIncome.php
@@ -3,8 +3,196 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\TaxRate;
+use App\TaxIncomeDetail;
+use Propaganistas\LaravelIntl\Facades\Currency as Currency;
+use Cache;
 
 class TaxIncome extends Model
 {
-    //
+    public function compute()
+    {
+        $computationType = $this->country->computation_type;
+
+        switch ($computationType) {
+            case 2: //Ladder
+                $taxRates = $this->getLadderTaxRates();
+                break;
+
+            default: //simple
+                $taxRates = $this->getSimpleTaxRates();
+        }
+
+        $standardTax        = $this->computeTax($taxRates->where('tax_type', 1)->all(), $computationType, $this->amount);
+        $taxBasedFromTax    = $this->computeTax($taxRates->where('tax_type', 2)->all(), $computationType, $standardTax);
+
+        return $standardTax + $taxBasedFromTax;
+    }
+
+    public function computeTax($taxRates, $computationType, $sourceAmount)
+    {
+        $totalTaxAmount = 0;
+
+
+        foreach ($taxRates as $taxRate) {
+            $taxAmount      = $this->getTaxAmount($taxRate, $computationType, $sourceAmount);
+            $totalTaxAmount = $totalTaxAmount + $taxAmount;
+
+            /*
+            $taxIncomeDetail                    = new TaxIncomeDetail;
+            $taxIncomeDetail->tax_income_id     = $this->id;
+            $taxIncomeDetail->tax_rate_id       = $taxRate->id;
+            $taxIncomeDetail->amount            = $taxAmount;
+            $taxIncomeDetail->save();
+            */
+        }
+
+        return $totalTaxAmount;
+    }
+
+    private function getSimpleTaxRates()
+    {
+        $taxRates = TaxRate::where('tax_category', $this->tax_category);
+        $taxRates = $this->queryByLocation($taxRates);
+        $taxRates = $taxRates
+        ->where('bracket_minimum', '<=', $this->amount)
+        ->where(function ($underOrNoMaximumAmount) {
+            $underOrNoMaximumAmount
+            ->where('bracket_maximum', '>=', $this->amount)
+            ->orWhere('bracket_maximum', null);
+        })
+        ->get();
+
+        return $taxRates;
+    }
+
+    private function getLadderTaxRates()
+    {
+        $taxRates = TaxRate::where('tax_category', $this->tax_category);
+        $taxRates = $this->queryByLocation($taxRates);
+        $taxRates = $taxRates
+        ->where(function ($ladderBracket) {
+            $ladderBracket
+            ->where(function ($normalBracket) {
+                $normalBracket
+                ->where('bracket_minimum', '<=', $this->amount)
+                ->where(function ($maximumLimits) {
+                    $maximumLimits->where(function ($withinOrNoMaximumAmount) {
+                        $withinOrNoMaximumAmount
+                        ->where('bracket_maximum', '>=', $this->amount)
+                        ->orWhere('bracket_maximum', null);
+                    })
+                    ->orWhere(function ($underOrNoMaximumAmount) {
+                        $underOrNoMaximumAmount
+                        ->where('bracket_maximum', '<=', $this->amount)
+                        ->orWhere('bracket_maximum', null);
+                    });
+                });
+            });
+        })
+        ->get();
+
+        return $taxRates;
+    }
+
+    private function queryByLocation($taxRates)
+    {
+        return $taxRates->where(function ($locationQuery) {
+            $locationQuery
+            ->where(function ($countryWide) {
+                $countryWide
+                ->where('country_id', $this->country_id)
+                ->whereNull('state_id')
+                ->whereNull('county_id');
+            })
+            ->orWhere(function ($stateWide) {
+                $stateWide
+                ->where('country_id', $this->country_id)
+                ->where('state_id', $this->state_id)
+                ->whereNull('county_id');
+            })
+            ->orWhere(function ($countyWide) {
+                $countyWide
+                ->where('country_id', $this->country_id)
+                ->where('state_id', $this->state_id)
+                ->where('county_id', $this->county_id);
+            });
+        });
+    }
+
+    private function getTaxAmount($taxRate, $computationType, $sourceAmount)
+    {
+        $total = 0;
+        //tax from fixed rate
+        if (!empty($taxRate->rate_fixed)) {
+            $total = $taxRate->rate_fixed;
+        }
+        //tax from percentage rate
+        if (!empty($taxRate->rate_percentage)) {
+            if ($computationType == 2) {
+                //ladder
+                if (empty($taxRate->bracket_maximum) || $sourceAmount < $taxRate->bracket_maximum) {
+                    $baseAmount = $sourceAmount - ($taxRate->bracket_minimum - 1);
+                } else {
+                    $baseAmount = $taxRate->bracket_maximum - ($taxRate->bracket_minimum - 1);
+                }
+                $total = $total + ($baseAmount * ($taxRate->rate_percentage / 100));
+            } else {
+                //simple
+                $total = $total + ($sourceAmount * ($taxRate->rate_percentage / 100));
+            }
+        }
+        return $total;
+    }
+
+    public function country()
+    {
+        return $this->belongsTo('App\Country');
+    }
+
+    public function state()
+    {
+        return $this->belongsTo('App\State');
+    }
+
+    public function county()
+    {
+        return $this->belongsTo('App\County');
+    }
+
+    public function getTotalTaxAttribute($value)
+    {
+        return Currency::format($value, $this->getCurrencyCode());
+    }
+
+    public function getAverageTaxAttribute($value)
+    {
+        return Currency::format($value, $this->getCurrencyCode());
+    }
+
+    public function getAverageTaxRateAttribute($value)
+    {
+        return number_format($value, 2);
+    }
+
+    public function getCountryAverageTaxRate()
+    {
+        return  number_format($this->average_country_tax  / ($this->average_raw_income / 100), 2) . ' %';
+    }
+
+    private function getCurrencyCode()
+    {
+        $country = '';
+        if (!empty($this->country)) {
+            $country = $this->country;
+        } elseif (!empty($this->state)) {
+            $country = $this->state->country;
+        } elseif (!empty($this->county)) {
+            $country = $this->county->country;
+        }
+
+        return Cache::rememberForever($country->id.'.currency_code', function () use ($country) {
+            return $country->currency_code;
+        });
+    }
 }
--- app/TaxRate.php
+++ app/TaxRate.php
@@ -3,8 +3,153 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
+use Cache;
+use Propaganistas\LaravelIntl\Facades\Currency as Currency;
 
 class TaxRate extends Model
 {
-    //
+    public function country()
+    {
+        return $this->belongsTo('App\Country');
+    }
+
+    public function state()
+    {
+        return $this->belongsTo('App\State');
+    }
+
+    public function county()
+    {
+        return $this->belongsTo('App\County');
+    }
+
+    public function formatTaxRate($fixed, $percentage, $currency = null)
+    {
+        if ($fixed > 0 && $percentage > 0) {
+            return Currency::format($fixed, $currency) . ' + ' . $percentage . '%';
+        }
+
+        if ($percentage > 0) {
+            return $percentage . '%';
+        }
+
+        if ($fixed > 0) {
+            return Currency::format($fixed, $currency);
+        }
+
+        return 'None';
+    }
+
+    public function arrayAverage($array)
+    {
+        $divisor = 0;
+        foreach ($array as $value) {
+            if ($value > 0) {
+                $divisor ++;
+            }
+        }
+
+        if ($divisor === 0) {
+            return 0;
+        }
+
+        return number_format(array_sum($array) / $divisor, 2);
+    }
+
+    public function getOverallAverageTaxRate($type, $country_id)
+    {
+        $taxes = [
+            $this->getCountryAverageTaxRate($type, $country_id),
+            $this->getStateAverageTaxRate($type, $country_id),
+            $this->getCountyAverageTaxRate($type, $country_id)
+        ];
+
+        $divisor = 0;
+        foreach ($taxes as $tax) {
+            if ($tax > 0) {
+                $divisor++;
+            }
+        }
+
+        if ($divisor == 0) {
+            return 0;
+        }
+
+        $average = array_sum($taxes) / $divisor;
+        return number_format($average, 2);
+    }
+
+    public function getCountryAverageTaxRate($type, $country_id)
+    {
+        $result = Cache::rememberForever($country_id . '.country.average.taxrate.' . $type, function () use ($country_id) {
+            return TaxRate::select(DB::raw('avg(rate_percentage) as average_tax_rate_percentage, avg(rate_fixed) as average_tax_rate_fixed, country_id'))
+                ->whereNull('state_id')
+                ->whereNull('county_id')
+                ->groupBy('country_id')
+                ->where('country_id', $country_id)
+                ->first();
+        });
+
+        if (empty($result)) {
+            return 0;
+        }
+
+        return $type == 'percentage' ? number_format($result->average_tax_rate_percentage, 2) : number_format($result->average_tax_rate_fixed, 2);
+    }
+
+    public function getStateAverageTaxRate($type, $country_id, $state_id = null)
+    {
+        $key = implode('-', [$country_id, $state_id]);
+        $result = Cache::rememberForever($key . '.state.average.taxrate.' . $type, function () use ($country_id, $state_id) {
+            $taxRate = TaxRate::select(DB::raw('avg(rate_percentage) as average_tax_rate_percentage, avg(rate_fixed) as average_tax_rate_fixed, country_id'))
+                ->groupBy('country_id')
+                ->whereNull('county_id')
+                ->where('country_id', $country_id);
+
+            if (!empty($state_id)) {
+                $taxRate->where('state_id', $state_id);
+            } else {
+                $taxRate->where('state_id', '!=', null);
+            }
+
+            return $taxRate->first();
+        });
+
+        if (empty($result)) {
+            return 0;
+        }
+
+        return $type == 'percentage' ? number_format($result->average_tax_rate_percentage, 2) : number_format($result->average_tax_rate_fixed, 2);
+    }
+
+    public function getCountyAverageTaxRate($type, $country_id, $state_id = null, $county_id = null)
+    {
+        $key = implode('-', [$country_id, $state_id, $county_id]);
+        $result = Cache::rememberForever($key . '.county.average.taxrate.' . $type, function () use ($country_id, $state_id, $county_id) {
+            $taxRate = TaxRate::select(DB::raw('avg(rate_percentage) as average_tax_rate_percentage, avg(rate_fixed) as average_tax_rate_fixed, country_id'))
+                ->groupBy('country_id')
+                ->where('country_id', $country_id);
+
+            if (!empty($state_id)) {
+                $taxRate->where('state_id', $state_id);
+            } else {
+                $taxRate->where('state_id', '!=', null);
+            }
+
+            if (!empty($county_id)) {
+                $taxRate->where('county_id', $county_id);
+            } else {
+                $taxRate->where('county_id', '!=', null);
+            }
+
+            return $taxRate->first();
+        });
+
+        if (empty($result)) {
+            return 0;
+        }
+
+        return $type == 'percentage' ? number_format($result->average_tax_rate_percentage, 2) : number_format($result->average_tax_rate_fixed, 2);
+    }
 }
